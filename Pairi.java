e41cd3e2657ff8e9c11d8072ca10a325ad325968
YeonjeeSong <93244406+YeonjeeSong@users.noreply.github.com>
2022-01-08 20:29:59 +0900

    Update Pairi.java

--- Pairi.java
+++ Pairi.java
@@ -8,7 +8,7 @@ public class Pairi extends PokemonDTO {
 	public void recharge() {
 		// TODO Auto-generated method stub
 		if(this.HP < 100) {
-			System.out.println("[system] : 파이리(이)가 불을 충전합니다. 파이리의 HP가 100% 충전되었습니다.");
+			System.out.println("[system] : 파이리가 불을 충전합니다. 파이리의 HP가 100% 충전되었습니다.");
 			this.HP = 100;
 		} else {
 			System.out.println("[system] : 파이리의 HP가 완충되어 있습니다. 공격을 진행해주세요.");
@@ -23,7 +23,7 @@ public class Pairi extends PokemonDTO {
 			System.out.println("[system] : 파이리가 불을 뿜습니다. 화르륵==33");
 			HP -= 30;
 			count++;
-			System.out.println("[system] : 파이리(이)가 " + count+"회 공격을 수행했습니다.");
+			System.out.println("[system] : 파이리가 " + count+"회 공격을 수행했습니다.");
 			System.out.println("[system] : 파이리의 현재 HP는 " + HP + "입니다." );
 			
 			if (mission == count) {
