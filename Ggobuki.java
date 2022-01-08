a525596929dd20fe7c284684fd70f255d3706724
Areum <92070292+moomining@users.noreply.github.com>
2022-01-08 20:23:35 +0900

    Update Ggobuki.java

--- Ggobuki.java
+++ Ggobuki.java
@@ -33,20 +33,21 @@ public class Ggobuki extends PokemonDTO {
 				
 				if (mission == count) {
 					Scanner sc = new Scanner(System.in);
-					System.out.print("[system] : 진화 미션 횟수를 모두 채우셨습니다. 진화를 진행하시겠습니까? (y/n) : ");
+					while(true) {
+					System.out.print("진화 미션 회수를 모두 채우셨습니다. 진화를 진행하시겠습니까? (y/n) : ");
 					String yesOrNo = sc.nextLine();
-					label:
-					if(yesOrNo.equals("y")) {
-						System.out.println("[system] : 꼬부기(이)가 어니부기(으)로 진화했습니다. 축하드립니다.");
-						return;
-					} else if (yesOrNo.equals("n")) {
-						System.out.println("[system] : 진화를 못해 화가난 꼬부기(이)가 해적이 되었습니다. 게임을 종료합니다.");
-						return;
-					} else {
-						System.out.println("[system] : y/n 중 하나만을 입력해야합니다. 다시 입력해주세요.");
-						break label;
-					}
 					
+					if(yesOrNo.equals("y")) {
+						System.out.println("꼬부기가 어니부기로 진화했습니다. 축하드립니다.");
+						System.exit(0);
+					} else if (yesOrNo.equals("n")) {
+						System.out.println("프로그램을 종료합니다.");
+						System.exit(0);
+					} else {
+						System.out.println("y/n 중 하나만을 입력해야합니다. 다시 입력해주세요.");
+						
+					}
+					}
 				}
 			} else { 
 				System.out.println("[system] : 꼬부기의 hp가 모자라 공격이 불가합니다. 충전을 진행하세요!");
