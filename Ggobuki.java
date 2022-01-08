fd3ea0ee0b4e7f6440df0f5eddc1e69daf1349d3
Areum <92070292+moomining@users.noreply.github.com>
2022-01-08 20:18:14 +0900

    Update Ggobuki.java

--- Ggobuki.java
+++ Ggobuki.java
@@ -25,7 +25,7 @@ public class Ggobuki extends PokemonDTO {
 	public void attack() {
 		
 			if (HP >= 50) { 
-				System.out.println("[system] : 꼬부기(이)가 물대포를 사용합니다! 꼬북꼬북!!!!!");
+				System.out.println("[system] : 꼬부기가 물을 뿜습니다. 뿌우우==33");
 				HP -= 30;
 				count++;
 				System.out.println("[system] : 꼬부기(이)가 " + count+"회 공격을 시행했습니다.");
