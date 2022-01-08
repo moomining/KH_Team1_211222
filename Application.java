0a8ee70c09e9cad5a66c8c046d6d4f9e84a9c836
Areum <92070292+moomining@users.noreply.github.com>
2022-01-08 17:50:04 +0900

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,71 @@
+package pokemonAdeventure;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("게임을 함께할 포켓몬을 선택해 주세요. ");
+		System.out.print("1. 피카츄 2. 파이리 3. 꼬부기 : ");
+		int pokeSelect = sc.nextInt();
+		sc.nextLine();
+		String poke = "";
+		switch (pokeSelect) {
+		case 1 : 
+			poke = "피카츄";
+			Pikachu pika = new Pikachu();
+			while (true) {
+				System.out.println(poke + "가 수행할 동작 고르기 : ");
+				System.out.print("1. 충전 / 2. 공격 / 9. 게임 종료 (번호 입력) : ");
+				int num = sc.nextInt();
+				switch (num) {
+					case 1 : pika.recharge(); break;
+					case 2 : pika.attack(); break;
+					case 9 : System.out.println("게임을 종료합니다."); return;
+					default : System.out.println("잘못 입력하셨습니다. 다시 1,2,9 중 하나의 번호를 입력해주세요."); break;
+					}
+			}
+		case 2 :
+			poke = "파이리";
+			Pairi pai = new Pairi();
+			while (true) {
+				System.out.println(poke + "가 수행할 동작 고르기 : ");
+				System.out.print("1. 충전 / 2. 공격 / 9. 게임 종료 (번호 입력) : ");
+				int num2 = sc.nextInt();
+					switch (num2) {
+					case 1 : pai.recharge(); break;
+					case 2 : pai.attack(); break;
+					case 9 : System.out.println("게임을 종료합니다."); return;
+					default : System.out.println("잘못 입력하셨습니다. 다시 1,2,9 중 하나의 번호를 입력해주세요."); break;
+					}
+			}
+		
+		case 3 :
+			poke = "꼬부기";
+			Ggobuki ggobuk = new Ggobuki();
+			while (true) {
+				System.out.println(poke + "가 수행할 동작 고르기 : ");
+				System.out.print("1. 충전 / 2. 공격 / 9. 게임 종료 (번호 입력) : ");
+				int num3 = sc.nextInt();
+				
+				switch (num3) {
+					case 1 : ggobuk.recharge(); break;
+					case 2 : ggobuk.attack(); break;
+					case 9 : System.out.println("게임을 종료합니다."); return;
+					default : System.out.println("잘못 입력하셨습니다. 다시 1,2,9 중 하나의 번호를 입력해주세요."); break;
+				}
+				
+			}	
+		}
+		
+		
+	}
+
+	public void menuSelect() {
+		
+		
+	}
+}
---
+++ Pikachu.java
@@ -0,0 +1,61 @@
+package pokemonAdeventure;
+
+import java.util.Scanner;
+
+public class Pikachu extends PokemonDTO {
+
+	@Override
+	public void recharge() {
+		// TODO Auto-generated method stub
+		if(this.HP < 100) {
+			System.out.println("피카츄의 HP가 100% 충전되었습니다.");
+			this.HP = 100;
+		} else {
+			System.out.println("피카츄의 HP가 완충되어 있습니다. 2를 눌러 공격을 진행해주세요.");
+			
+		}
+		
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("피카츄에게 부여된 미션 회수는 " + mission +"회입니다." );
+		if (HP >= 50) { 
+			System.out.println("피카츄가 전기를 쏩니다. 치지직===ZZ");
+			HP -= 30;
+			count++;
+			System.out.println("피카츄가 " + count+"회 공격을 시행했습니다.");
+			System.out.println("피카츄의 현재 HP는 " + HP + "입니다." );
+			
+			if (mission == count) {
+				Scanner sc = new Scanner(System.in);
+				System.out.print("진화 미션 회수를 모두 채우셨습니다. 진화를 진행하시겠습니까? (y/n) : ");
+				String yesOrNo = sc.nextLine();
+				label:
+				if(yesOrNo.equals("y")) {
+					System.out.println("피카츄가 라이츄로 진화했습니다. 축하드립니다.");
+					return;
+				} else if (yesOrNo.equals("n")) {
+					System.out.println("프로그램을 종료합니다.");
+					return;
+				} else {
+					System.out.println("y/n 중 하나만을 입력해야합니다. 다시 입력해주세요.");
+					break label;
+				}
+				
+			}
+		} else { 
+			System.out.println("충전이 필요합니다. 충전을 시행합니다.");
+			recharge();
+		}
+		
+	}
+
+	@Override
+	public void PokemonDTO() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
