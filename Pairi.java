45b89ddd1dc6555924aab1dc44b3699410957ace
gowls8318 <68188378+gowls8318@users.noreply.github.com>
2022-01-08 20:12:05 +0900

    Update Pairi.java

--- Pairi.java
+++ Pairi.java
@@ -8,7 +8,7 @@ public class Pairi extends PokemonDTO {
 	public void recharge() {
 		// TODO Auto-generated method stub
 		if(this.HP < 100) {
-			System.out.println("[system] : 파이리가 불을 충전합니다. 파이리의 HP가 100% 충전되었습니다.");
+			System.out.println("[system] : 파이리(이)가 불을 충전합니다. 파이리의 HP가 100% 충전되었습니다.");
 			this.HP = 100;
 		} else {
 			System.out.println("[system] : 파이리의 HP가 완충되어 있습니다. 공격을 진행해주세요.");
